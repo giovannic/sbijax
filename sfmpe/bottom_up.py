89f3a0ea879155bb1e7e5579520b866dea4e2178
Giovanni Charles <giovanni.charles@gmail.com>
2025-06-06 13:05:11 +0200

    Start refactor of bottom up

---
+++ sfmpe/bottom_up.py
@@ -0,0 +1,212 @@
+from sfmpe.util.dataloader import (
+    flatten_structured,
+    flat_as_batch_iterators,
+    pad_multidim_event,
+    combine_data
+)
+
+from jax import numpy as jnp, random as jr, vmap, tree
+
+def train_bottom_up(
+    key,
+    estim,
+    prior_fn,
+    simulator_fn,
+    global_names,
+    local_names,
+    n_local,
+    n_rounds,
+    n_simulations,
+    n_epochs,
+    y_observed,
+    independence
+    ):
+
+    data = {}
+    flat_data, data_slices = (None, {})
+    train_data = None
+    for _ in range(n_rounds):
+        # Fit p(z|theta, y)
+        theta_key, obs_key, key = jr.split(key, 3)
+        if flat_data is not None and data_slices:
+            flat_y_obs, data_slices = flatten_structured(
+                {
+                    'theta': data['theta'],
+                    'y': y_observed
+                },
+                independence=independence
+            )
+            theta_samples_full = estim.sample_structured_posterior(
+                theta_key,
+                flat_y_obs['data']['y'],
+                flat_data['labels'], #type: ignore
+                data_slices['theta'], #type: ignore
+                masks=flat_data['masks'], #type: ignore
+                n_samples=n_simulations
+            )
+            theta_samples = {}
+            for g in global_names:
+                theta_samples[g] = theta_samples_full[g]
+            for l in local_names:
+                choice_key, theta_key = jr.split(theta_key)
+                theta_samples[l] = jr.choice(
+                    choice_key,
+                    theta_samples_full[l],
+                    shape=(1,),
+                    axis=1
+                )
+        else:
+            theta_samples = prior_fn(1).sample(
+                seed=theta_key,
+                sample_shape=(n_simulations,)
+            )
+
+        y_samples= simulator_fn(obs_key, theta_samples)
+
+        z_data = {
+            'theta': {
+                l: theta_samples[l] #type: ignore
+                for l in local_names
+            },
+            'y': {
+                'obs': y_samples['obs']
+            } | {
+                g: theta_samples[g] #type: ignore
+                for g in global_names
+            }
+        }
+
+        z_flat, _ = flatten_structured(
+            z_data,
+            independence=independence
+        )
+
+        if train_data is None:
+            train_data = z_flat
+        else:
+            train_data = combine_data(train_data, z_flat)
+
+        itr_key, key = jr.split(key)
+        train_iter, val_iter = flat_as_batch_iterators(
+            itr_key,
+            train_data
+        )
+
+        fit_key, key = jr.split(key)
+
+        estim.fit(
+            fit_key,
+            train_iter,
+            val_iter,
+            n_iter=n_epochs
+        )
+
+        # simulate from p(z_vec|theta, y_vec)
+        z_sim = z_data.copy()
+        sim_key, key = jr.split(key)
+        z_sim['y']['obs'] = vmap(
+            lambda k: jr.choice(k, z_data['y']['obs'], shape=(n_local,)) # type: ignore
+        )(jr.split(sim_key, n_simulations))
+
+        # pad z to n_z
+        for l in local_names:
+            z_sim['theta'][l] = pad_multidim_event(
+                z_sim['theta'][l],
+                1,
+                (n_local,)
+            )
+
+        (
+            flat_z_sim,
+            z_sim_slices
+        ) = flatten_structured(
+            z_sim,
+            independence=independence
+        )
+
+        sample_key, key = jr.split(key)
+
+        z_vec = vmap(
+            lambda key, obs: tree.map(
+                lambda leaf: leaf[0], #TODO: clean up
+                estim.sample_structured_posterior(
+                    key,
+                    jnp.expand_dims(obs, 0),
+                    flat_z_sim['labels'],
+                    z_sim_slices['theta'],
+                    masks=flat_z_sim['masks'],
+                    n_samples=1
+                )
+            )
+        )(jr.split(sample_key, n_simulations), flat_z_sim['data']['y'])
+
+        # fit p(theta,z_vec|y_vec)
+        data = {
+            'theta': {
+                'z': z_vec['z'],
+                'theta': z_sim['y']['theta']
+            },
+            'y': {
+                'obs': z_sim['y']['obs']
+            }
+        }
+
+        flat_data, data_slices = flatten_structured(
+            data,
+            independence=independence
+        )
+
+        train_data = tree.map(
+            lambda leaf: leaf[:n_simulations],
+            combine_data(train_data, flat_data)
+        )
+
+        train_data = combine_data(train_data, flat_data)
+
+        train_key, itr_key, key = jr.split(key, 3)
+        train_iter, val_iter = flat_as_batch_iterators(
+            itr_key,
+            train_data
+        )
+
+        estim.fit(
+            train_key,
+            train_iter,
+            val_iter,
+            n_iter=n_epochs
+        )
+
+        # TODO: refactor into structuring code or estimator
+        return (
+            flat_data['labels'],
+            data_slices['theta'],
+            flat_data['masks']
+        )
+
+
+def get_posterior(
+    key,
+    estim,
+    labels,
+    slices,
+    masks,
+    n_samples,
+    theta_truth,
+    y_observed,
+    independence
+    ):
+    obs_flat, _ = flatten_structured(
+        { 'theta': theta_truth, 'y': y_observed },
+        independence=independence
+    )
+
+    posterior = estim.sample_structured_posterior( 
+        key,
+        obs_flat['data']['y'],
+        labels,
+        slices,
+        masks=masks,
+        n_samples=n_samples
+    )
+
+    return posterior
--- test/test_hierarchical.py
+++ test/test_hierarchical.py
@@ -1,16 +1,11 @@
 import pytest
-from jax import numpy as jnp, random as jr, vmap, tree
+from jax import numpy as jnp, random as jr
 from tensorflow_probability.substrates.jax import distributions as tfd
 
 from sfmpe.fmpe import SFMPE
+from sfmpe.bottom_up import train_bottom_up, get_posterior
 from sfmpe.nn.make_continuous_flow import CNF
 from sfmpe.nn.transformer.transformer import Transformer
-from sfmpe.util.dataloader import (
-    flatten_structured,
-    flat_as_batch_iterators,
-    pad_multidim_event,
-    combine_data
-)
 
 from sbijax import FMPE
 from sbijax.nn import make_cnf
@@ -89,145 +84,34 @@ def test_hierarchical_parameters():
 
     estim = SFMPE(model)
 
-    flat_data, data_slices = (None, {})
-    train_data = None
-    for _ in range(n_rounds):
-        # Fit p(z|theta, y)
-        theta_key, obs_key, key = jr.split(key, 3)
-        if flat_data is not None and data_slices:
-            choice_key, theta_key = jr.split(theta_key)
-            flat_y_obs, data_slices = flatten_structured(
-                {
-                    'theta': data['theta'], #type: ignore
-                    'y': y_observed
-                },
-                independence=independence
-            )
-            theta_samples = estim.sample_structured_posterior(
-                theta_key,
-                flat_y_obs['data']['y'],
-                flat_data['labels'], #type: ignore
-                data_slices['theta'], #type: ignore
-                masks=flat_data['masks'], #type: ignore
-                n_samples=n_simulations
-            )
-            theta_samples = {
-                'theta': theta_samples['theta'],
-                'z': jr.choice(choice_key, theta_samples['z'], shape=(1,), axis=1)
-            }
-        else:
-            theta_samples = prior_fn(1).sample(seed=theta_key, sample_shape=(n_simulations,))
+    train_key, key = jr.split(key)
+    labels, slices, masks = train_bottom_up(
+        train_key,
+        estim,
+        prior_fn,
+        simulator_fn,
+        ['theta'],
+        ['z'],
+        n_z,
+        n_rounds,
+        n_simulations,
+        n_epochs,
+        y_observed,
+        independence
+    )
 
-        y_samples= simulator_fn(obs_key, theta_samples)
-
-        z_data = {
-            'theta': {
-                'z': theta_samples['z'], #type: ignore
-            },
-            'y': {
-                'theta': theta_samples['theta'], #type: ignore
-                'obs': y_samples['obs']
-            }
-        }
-
-        z_flat, _ = flatten_structured(
-            z_data,
-            independence=independence
-        )
-
-        if train_data is None:
-            train_data = z_flat
-        else:
-            train_data = combine_data(train_data, z_flat)
-
-        itr_key, key = jr.split(key)
-        train_iter, val_iter = flat_as_batch_iterators(
-            itr_key,
-            train_data
-        )
-
-        fit_key, key = jr.split(key)
-
-        estim.fit(
-            fit_key,
-            train_iter,
-            val_iter,
-            n_iter=n_epochs
-        )
-
-        # simulate from p(z_vec|theta, y_vec)
-        z_sim = z_data.copy()
-        sim_key, key = jr.split(key)
-        z_sim['y']['obs'] = vmap(
-            lambda k: jr.choice(k, z_data['y']['obs'], shape=(n_z,)) # type: ignore
-        )(jr.split(sim_key, n_simulations))
-
-        # pad z to n_z
-        z_sim['theta']['z'] = pad_multidim_event(
-            z_sim['theta']['z'],
-            1,
-            (n_z,)
-        )
-
-        (
-            flat_z_sim,
-            z_sim_slices
-        ) = flatten_structured(
-            z_sim,
-            independence=independence
-        )
-
-        sample_key, key = jr.split(key)
-
-        z_vec = vmap(
-            lambda key, obs: tree.map(
-                lambda leaf: leaf[0], #TODO: clean up
-                estim.sample_structured_posterior(
-                    key,
-                    jnp.expand_dims(obs, 0),
-                    flat_z_sim['labels'],
-                    z_sim_slices['theta'],
-                    masks=flat_z_sim['masks'],
-                    n_samples=1
-                )
-            )
-        )(jr.split(sample_key, n_simulations), flat_z_sim['data']['y'])
-
-        # fit p(theta,z_vec|y_vec)
-        data = {
-            'theta': {
-                'z': z_vec['z'],
-                'theta': z_sim['y']['theta']
-            },
-            'y': {
-                'obs': z_sim['y']['obs']
-            }
-        }
-
-        flat_data, data_slices = flatten_structured(
-            data,
-            independence=independence
-        )
-
-        train_data = tree.map(
-            lambda leaf: leaf[:n_simulations],
-            combine_data(train_data, flat_data)
-        )
-
-        train_data = combine_data(train_data, flat_data)
-
-        train_key, itr_key, key = jr.split(key, 3)
-        train_iter, val_iter = flat_as_batch_iterators(
-            itr_key,
-            train_data
-        )
-
-        estim.fit(
-            train_key,
-            train_iter,
-            val_iter,
-            n_iter=n_epochs
-        )
+    post_key, key = jr.split(key)
+    posterior = get_posterior(
+        post_key,
+        estim,
+        labels,
+        slices,
+        masks,
+        n_post_samples,
+        theta_truth,
+        y_observed,
+        independence
+    )
 
     print('sbijax')
 
@@ -281,19 +165,6 @@ def test_hierarchical_parameters():
             data=data,
             n_iter=n_epochs
         )
-
-    obs_flat, _ = flatten_structured(
-        { 'theta': theta_truth, 'y': y_observed },
-        independence=independence
-    )
-    posterior = estim.sample_structured_posterior( 
-        jr.PRNGKey(3),
-        obs_flat['data']['y'],
-        flat_data['labels'], #type: ignore
-        data_slices['theta'], #type: ignore
-        masks=flat_data['masks'], #type: ignore
-        n_samples=n_post_samples,
-    )
 
     sbijax_posterior, _ = sbijax_estim.sample_posterior(
         rng_key=jr.PRNGKey(3),
