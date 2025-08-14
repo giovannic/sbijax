343b9a153609c04f2061b0d18f58c8b8e41dce1e
Giovanni Charles <giovanni.charles@gmail.com>
2025-08-14 16:08:37 +0100

    Add script to run all examples

---
+++ examples/all.sh
@@ -0,0 +1,2 @@
+python examples/hierarchical_gaussian.py -m n_simulations=1000,5000,10000 n_rounds=1 n_epochs=1000
+python examples/hierarchical_gaussian.py -m n_simulations=5000 n_rounds=1 n_epochs=1000 n_theta=1,2,5,10,15,20
