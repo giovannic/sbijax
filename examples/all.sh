d9557af3415bada0e1350aa305165e4680afcd76
Giovanni <giovanni.charles@gmail.com>
2025-08-29 07:48:21 +0000

    Add brownian runs

--- examples/all.sh
+++ examples/all.sh
@@ -1,2 +1,4 @@
-python examples/hierarchical_gaussian.py -m n_simulations=1000,5000,10000 n_rounds=1 n_epochs=1000
-python examples/hierarchical_gaussian.py -m n_simulations=5000 n_rounds=1 n_epochs=1000 n_theta=1,2,5,10,20
+python examples/hierarchical_gaussian.py -m n_simulations=1000,5000,10000 n_rounds=1 n_epochs=1000 seed=0,1,2,3,4,5
+python examples/hierarchical_gaussian.py -m n_simulations=5000 n_rounds=1 n_epochs=1000 n_theta=1,2,5,10,20 seed=0,1,2,3,4,5
+python examples/hierarchical_brownian.py -m n_simulations=1000,5000,10000 n_rounds=1 n_epochs=1000 seed=0,1,2,3,4,5
+python examples/hierarchical_brownian.py -m n_simulations=5000 n_rounds=1 n_epochs=1000 n_theta=1,2,5,10,20 seed=0,1,2,3,4,5
